1d11e3da78c1d3e82d0e63d2129e3a3beda5d82b
Abuzahra <abuzahray44@gmail.com>
2023-05-18 10:54:39 +0300

    added a no cusines feature

--- virtual_resteraunts.py
+++ virtual_resteraunts.py
@@ -2,11 +2,6 @@ from trre_node import TreeNode
 from restaurantData import types, restaurant_data
 from naive_pattern_search import naive_pattern_search
 
-
-
-    
-
-
 def welcome():
     print("Welcome to SoHo Restuarants")
     print("What type of food would you like to eat?")
@@ -34,7 +29,10 @@ def searching_for_cuisuine():
         if naive_pattern_search(child.value, root.value):
             resualts.append(child)
     if len(resualts) == 1:
-        return resualts[0]  
+        return resualts[0]
+    elif len(resualts) == 0:
+        print('there is no cusines starting with this letter(s)')
+        return searching_for_cuisuine()
     else:
         print(f"With Those beginning letters, your choices are {[resault.value for resault in resualts]}")
         return searching_for_cuisuine()
